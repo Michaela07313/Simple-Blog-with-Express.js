402025033ec80bb54152d8be0e3f4255fa04cea9
Michaela07313 <miminka07313@gmail.com>
2017-03-30 13:19:56 +0300

    CRUD - articles

--- controllers/article.js
+++ controllers/article.js
@@ -44,5 +44,71 @@ module.exports = {
         .then(article => {
             res.render('article/details', article)
         })
+    },
+    editGet: (req, res) => {
+        let id = req.params.id
+
+        Article.findById(id)
+        .then(article => {
+            res.render('article/edit', article)
+        })
+    },
+    editPost: (req, res) => {
+        let articleArgs = req.body
+        let id = req.params.id
+        let errorMsg = ''
+
+        if (!req.isAuthenticated()) {
+            errorMsg = 'You should be logged in to edit the article!'
+        } else if (!articleArgs.title) {
+            errorMsg = 'Invalid title'
+        } else if (!articleArgs.content) {
+            errorMsg = 'Invalid content'
+        }
+
+        if (errorMsg) {
+            res.render('article/edit', {error: errorMsg})
+        } else {
+          Article.findByIdAndUpdate(id, {
+          $set: {
+            title: articleArgs.title,
+            content: articleArgs.content
+          },
+          new: true
+        })
+        .exec()
+        .then(articleArgs => {
+          res.redirect(`/article/details/${id}`)
+        })
+      }
+    },
+    deleteGet: (req, res) => {
+        let id = req.params.id
+
+        Article.findById(id)
+        .then(article => {
+            res.render('article/delete', article)
+        })
+    },
+    deletePost: (req, res) => {
+        let id = req.params.id
+
+        Article.findOneAndRemove({_id: id})
+        .populate('author')
+        .then(article => {
+            let author = article.author
+            let index = author.articles.indexOf(article.id)
+
+            if (index < 0) {
+                let errorMsg = 'Article was not found for that author'
+                res.render('article/delete', {error: errorMsg})
+            } else {
+                let count = 1
+                author.articles.splice(index, count)
+                author.save().then((user) => {
+                    res.redirect('/')
+                })
+            }
+        })
     }
 }
